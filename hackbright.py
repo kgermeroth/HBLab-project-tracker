c016d3f4638201c7b37c7bd0020c0a5756e9e1c9
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-30 16:40:53 -0700

    completed function get_project_by_title

--- hackbright.py
+++ hackbright.py
@@ -58,7 +58,18 @@ def make_new_student(first_name, last_name, github):
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+    
+    QUERY = """
+        SELECT title, description
+        FROM projects
+        WHERE title = :title
+        """
+
+    db_cursor = db.session.execute(QUERY, {'title' : title})
+
+    row = db_cursor.fetchone()
+
+    print(f"{title} information: {row[1]}")
 
 
 def get_grade_by_github_title(github, title):
@@ -102,7 +113,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    handle_input()
+    # handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
